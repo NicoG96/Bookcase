96d48c2b90086361e2616d528029b40542484cb2
Nico Gonzalez <nicog96@gmail.com>
2019-03-24 21:18:58 -0400

    Create new activity to display the book details fragment

---
+++ app/src/main/java/com/example/bookcase/bookDetails.java
@@ -0,0 +1,12 @@
+package com.example.bookcase;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class bookDetails extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_book_details);
+    }
+}
