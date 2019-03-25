9d155e951872b80044874222caa7a70cca93c994
Nico Gonzalez <nicog96@gmail.com>
2019-03-24 20:34:42 -0400

    Override implemented onBookSelected listener

--- app/src/main/java/com/example/bookcase/MainActivity.java
+++ app/src/main/java/com/example/bookcase/MainActivity.java
@@ -3,11 +3,18 @@ package com.example.bookcase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BookListFragment.onBookSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBookSelected(int position) {
+        BookDetailsFragment book = (BookDetailsFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_book_details);
+        assert book != null;
+        book.displayBook(position);
     }
 }
