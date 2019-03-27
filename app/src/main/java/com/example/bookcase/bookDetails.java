5482053dca26c41eb4eb1f907d7ca8c12fa260ef
Nico Gonzalez <nicog96@gmail.com>
2019-03-27 04:51:50 -0400

    Remove unused library

--- app/src/main/java/com/example/bookcase/bookDetails.java
+++ app/src/main/java/com/example/bookcase/bookDetails.java
@@ -1,6 +1,7 @@
 package com.example.bookcase;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
