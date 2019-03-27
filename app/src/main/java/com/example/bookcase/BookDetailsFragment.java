478ed96e7a2e3bc260fbaedfe60d9da99098778b
Nico Gonzalez <nicog96@gmail.com>
2019-03-27 00:14:13 -0400

    Draft the logic for extracting the desired book and setting it in the textview

--- app/src/main/java/com/example/bookcase/BookDetailsFragment.java
+++ app/src/main/java/com/example/bookcase/BookDetailsFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class BookDetailsFragment extends Fragment {
 
@@ -12,6 +13,23 @@ public class BookDetailsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_book_details, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_book_details, container, false);
+
+        //get the textview object from xml
+        TextView bookName = rootView.findViewById(R.id.book_title);
+        assert getArguments() != null;
+
+        //find the book referenced
+        int index = getArguments().getInt("index");
+
+        //set the textview to the name of that book
+        bookName.setText(displayBook(index));
+        return rootView;
+    }
+
+    /** NEEDS WORK */
+    public String displayBook(int position) {
+        String[] books = getResources().getStringArray(R.array.books);
+        return books[position];
     }
 }
