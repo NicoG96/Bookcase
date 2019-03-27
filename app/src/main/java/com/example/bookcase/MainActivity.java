194ca3c60e0762453a0f7f070002ced02c2cd9f5
Nico Gonzalez <nicog96@gmail.com>
2019-03-27 04:53:43 -0400

    Add if-statement checking device orientation
    shortcuts redundant code that relies on fragment adapters as we are only manipulating 1 fragment

--- app/src/main/java/com/example/bookcase/MainActivity.java
+++ app/src/main/java/com/example/bookcase/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.bookcase;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements BookListFragment.onBookSelectedListener {
 
@@ -14,12 +16,22 @@ public class MainActivity extends AppCompatActivity implements BookListFragment.
 
     @Override
     public void onBookSelected(int position) {
-        //create intent to open the book details window
-        Intent intent = new Intent(this, bookDetails.class);
+        //get the orientation of the device before doing anything
+        int orientation = getResources().getConfiguration().orientation;
 
-        //send chosen book along with intent
-        intent.putExtra("book", position);
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            //create intent to open the book details window
+            Intent intent = new Intent(this, bookDetails.class);
 
-        startActivity(intent);
+            //send chosen book along with intent
+            intent.putExtra("book", position);
+
+            startActivity(intent);
+        } else {
+            BookDetailsFragment bdf = (BookDetailsFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_book_details);
+            assert bdf != null;
+            //TextView test = findViewById(R.id.book_title);
+            //test.setText(bdf.displayBook(position));
+        }
     }
 }
