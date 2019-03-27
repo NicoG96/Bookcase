db2d1cef817806cd1b5e7686f2bf08f8f7171867
Nico Gonzalez <nicog96@gmail.com>
2019-03-27 04:52:37 -0400

    Surround core onCreateView code with an if-statement checking device orientation to avoid app crash when in landscape

--- app/src/main/java/com/example/bookcase/BookDetailsFragment.java
+++ app/src/main/java/com/example/bookcase/BookDetailsFragment.java
@@ -1,5 +1,6 @@
 package com.example.bookcase;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -15,15 +16,18 @@ public class BookDetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_book_details, container, false);
 
-        //get the textview object from xml
-        TextView bookName = rootView.findViewById(R.id.book_title);
-        assert getArguments() != null;
+        int orientation = getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            //get the textview object from xml
+            TextView bookName = rootView.findViewById(R.id.book_title);
+            assert getArguments() != null;
 
-        //find the book referenced
-        int index = getArguments().getInt("index");
+            //find the book referenced
+            int index = getArguments().getInt("index");
 
-        //set the textview to the name of that book
-        bookName.setText(displayBook(index));
+            //set the textview to the name of that book
+            bookName.setText(displayBook(index));
+        }
         return rootView;
     }
 
