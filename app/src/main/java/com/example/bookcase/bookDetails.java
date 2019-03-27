1e85f7640d06a61e42dc472a8fd41b9adc678cf9
Nico Gonzalez <nicog96@gmail.com>
2019-03-27 01:40:25 -0400

    Extract intent that was passed from main activity and send to the pager adapter for further use

--- app/src/main/java/com/example/bookcase/bookDetails.java
+++ app/src/main/java/com/example/bookcase/bookDetails.java
@@ -1,5 +1,6 @@
 package com.example.bookcase;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
@@ -14,11 +15,17 @@ public class bookDetails extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pager);
 
+        //get the intent from the click
+        Intent intent = getIntent();
+
         //Instantiate a ViewPager from the xml item
         vPager = findViewById(R.id.pager);
 
         //instantiate a new page adapter (uses the fragment class to create objects for vPager)
         pAdapter = new FragmentCollectionAdapter(getSupportFragmentManager());
+
+        //assign the intent to a class variable in the adapter
+        pAdapter.bookSelected = intent.getIntExtra("book", 0);
 
         //set the pager to use this adapter containing the pages
         vPager.setAdapter(pAdapter);
