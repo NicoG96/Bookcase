74f82cc0b13fee244fbfbb31e478b83ac140110e
Nico Gonzalez <nicog96@gmail.com>
2019-03-30 21:50:19 -0400

    Add setter methods

--- app/src/main/java/com/example/bookcase/Book.java
+++ app/src/main/java/com/example/bookcase/Book.java
@@ -15,6 +15,28 @@ public class Book {
         this.coverURL = coverURL;
     }
 
+    public Book() {}
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCoverURL(String coverURL) {
+        this.coverURL = coverURL;
+    }
+
+    public void setPublished(int published) {
+        this.published = published;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     @Override
     public String toString() {
         return "Book ID:\t" + id + "\n" +
