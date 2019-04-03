937ca74c4c4d96c3f7a8ed8a6c7cafc96dc50c7c
Nico Gonzalez <nicog96@gmail.com>
2019-04-03 00:44:07 -0400

    Transfer background thread to booklist fragment

--- app/src/main/java/com/example/bookcase/BookListFragment.java
+++ app/src/main/java/com/example/bookcase/BookListFragment.java
@@ -3,6 +3,7 @@ package com.example.bookcase;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,9 +11,19 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 import static com.example.bookcase.MainActivity.library;
 
 public class BookListFragment extends Fragment {
+    ArrayAdapter<Book> adapter;
+    ListView booklist;
+
     public BookListFragment() {}
     onBookSelectedListener callback;
 
@@ -45,10 +56,10 @@ public class BookListFragment extends Fragment {
         assert ctx != null;
 
         //find the listview and create its corresponding object
-        ListView booklist = v.findViewById(R.id.book_list);
+        booklist = v.findViewById(R.id.book_list);
 
         //create the adapter that will display the books
-        ArrayAdapter<Book> adapter = new ArrayAdapter<>(ctx, android.R.layout.simple_list_item_1, library);
+        adapter = new ArrayAdapter<>(ctx, android.R.layout.simple_list_item_1, library);
 
         //load that adapter into the listview
         booklist.setAdapter(adapter);
@@ -62,5 +73,63 @@ public class BookListFragment extends Fragment {
             }
         });
         return v;
+    }
+
+    public void getBooks(final String site) {
+        //thread to parse JSON data from website
+        Thread t = new Thread(){
+            @Override
+            public void run(){
+                URL resource;
+
+                try {
+                    resource = new URL(site);
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(resource.openStream()));
+
+                    //variables to hold the website content
+                    StringBuilder response = new StringBuilder();
+                    String tmpResponse;
+
+                    tmpResponse = reader.readLine();
+
+                    //keep reading until end of page
+                    while (tmpResponse != null) {
+                        response.append(tmpResponse);
+                        tmpResponse = reader.readLine();
+                    }
+
+                    //create a JSON array object from this data
+                    JSONArray arr = new JSONArray(response.toString());
+
+                    //getBooks() called for booklist update, so we have to clear old library
+                    if(!library.isEmpty()) {
+                        library.clear();
+                    }
+
+                    //now transfer the members of this array into an array of book objects
+                    for(int i = 0; i < arr.length(); i++) {
+                        //make new book
+                        final Book book = new Book();
+
+                        //get the book from the JSON array
+                        JSONObject jBook = arr.getJSONObject(i);
+
+                        //get the values from that book and load them into the Book object
+                        book.setId(jBook.getInt("book_id"));
+                        book.setAuthor(jBook.getString("author"));
+                        book.setPublished(jBook.getInt("published"));
+                        book.setTitle(jBook.getString("title"));
+                        //book.setCoverURL(jBook.getString("cover_url"));
+
+                        //then finally add that book to the library array
+                        library.add(book);
+                    }
+
+                } catch (Exception e){
+                    Log.e("JSONException", "Error: " + e.toString());
+                }
+            }
+        };
+        t.start();
     }
 }
