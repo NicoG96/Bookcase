b7b2c54098ecc89acdbd083be1d341559ae44d89
Nico Gonzalez <nicog96@gmail.com>
2019-04-24 22:49:08 -0400

    Remove async for progress bar, replace with handler thread calls

--- app/src/main/java/com/example/bookcase/BookDetailsFragment.java
+++ app/src/main/java/com/example/bookcase/BookDetailsFragment.java
@@ -1,10 +1,13 @@
 package com.example.bookcase;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,7 +18,13 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import static com.example.bookcase.MainActivity.library;
 
 public class BookDetailsFragment extends Fragment {
@@ -35,16 +44,27 @@ public class BookDetailsFragment extends Fragment {
 
     //delete/download button
     Button dl_btn;
+    SeekBar dl_progress;
 
     public BookDetailsFragment() {}
 
     onAudioActionListener callback;
 
+    @SuppressLint("HandlerLeak")
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            int pos = msg.what;
+            seeker.setProgress(pos);
+        }
+    };
+
     public interface onAudioActionListener {
         void playBook(int book_id);
         void pauseBook();
         void stopBook();
-        void setprogress(int position, boolean fromUser);
+        void setProgHand(Handler handler);
+        void setProgress(int position, boolean fromUser);
     }
 
     @Override
@@ -84,6 +104,7 @@ public class BookDetailsFragment extends Fragment {
 
         //get download/delete button
         dl_btn = rootView.findViewById(R.id.download_btn);
+        dl_progress = rootView.findViewById(R.id.dl_progress);
 
         //TRY to get a passed index if there is one
         try {
@@ -100,9 +121,14 @@ public class BookDetailsFragment extends Fragment {
         play_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                callback.playBook(book.getId());
                 seeker.setMax(book.getDuration());
-                new updateSeeker(seeker).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+                callback.playBook(book.getId());
+                seeker.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        callback.setProgHand(handler);
+                    }
+                });
             }
         });
 
@@ -113,12 +139,12 @@ public class BookDetailsFragment extends Fragment {
                 if(dl_btn.getText().toString().equals("Download")) {
                     String query = "https://kamorris.com/lab/audlib/download.php?id=" + Integer.toString(book.getId());
 
-                    //update button text to communicate to the user that the audiobook has downloaded
-                    dl_btn.setText(R.string.delete_txt);
+                    //download in background
+                    new downloadBook(dl_progress, dl_btn).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, query);
 
                 //otherwise it's a delete button
                 } else {
-
+                    //DELETION STUFFS
                     //update button text to communicate to the user that the audiobook has been deleted
                     dl_btn.setText(R.string.download_txt);
                 }
@@ -129,6 +155,12 @@ public class BookDetailsFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 callback.pauseBook();
+                seeker.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        callback.setProgHand(handler);
+                    }
+                });
             }
         });
 
@@ -136,14 +168,19 @@ public class BookDetailsFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 callback.stopBook();
-                seeker.setProgress(0);
+                seeker.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        callback.setProgHand(handler);
+                    }
+                });
             }
         });
 
         seeker.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                callback.setprogress(progress, fromUser);
+                callback.setProgress(progress, fromUser);
             }
 
             @Override
@@ -205,33 +242,95 @@ public class BookDetailsFragment extends Fragment {
         }
     }
 
-    private static class updateSeeker extends AsyncTask<Void, Integer, Void> {
-        SeekBar seeker;
+    private static class downloadBook extends AsyncTask<String, Integer, Void> {
+        SeekBar dl_progress;
+        Button dl_btn;
 
-        public updateSeeker(SeekBar seeker) {
-            this.seeker = seeker;
+        public downloadBook(SeekBar dl_progress, Button dl_btn) {
+            this.dl_progress = dl_progress;
+            this.dl_btn = dl_btn;
         }
 
         @Override
-        protected Void doInBackground(Void... arg0) {
-            for(int i = 0; i < seeker.getMax(); i++) {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            //make download progress bar visible
+            dl_progress.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected Void doInBackground(String... sUrl) {
+            InputStream input = null;
+            OutputStream output = null;
+            HttpURLConnection connection = null;
+            try {
+                URL url = new URL(sUrl[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                // expect HTTP 200 OK, so we don't mistakenly save error report
+                // instead of the file
+                if(connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                    System.out.println("Server returned HTTP " + connection.getResponseCode()
+                            + " " + connection.getResponseMessage());
                 }
-                publishProgress(i);
+
+                // this will be useful to display download percentage
+                // might be -1: server did not report the length
+                int fileLength = connection.getContentLength();
+
+                // download the file
+                input = connection.getInputStream();
+                output = new FileOutputStream("/sdcard/file_name.extension");
+
+                byte data[] = new byte[4096];
+                long total = 0;
+                int count;
+                while((count = input.read(data)) != -1) {
+                    // allow canceling with back button
+                    if(isCancelled()) {
+                        input.close();
+                        return null;
+                    }
+                    total += count;
+                    // publishing the progress....
+                    if(fileLength > 0) // only if total length is known
+                        publishProgress((int) (total * 100 / fileLength));
+                    output.write(data, 0, count);
+                }
+            } catch(Exception e) {
+                System.out.println(e.toString());
+            } finally {
+                try {
+                    if(output != null)
+                        output.close();
+                    if(input != null)
+                        input.close();
+                } catch(IOException ignored) {
+                }
+
+                if(connection != null)
+                    connection.disconnect();
             }
             return null;
         }
 
+
+
         @Override
         protected void onProgressUpdate(Integer... progress) {
-            seeker.setProgress(progress[0]);
+            super.onProgressUpdate(progress);
+            dl_progress.setProgress(progress[0]);
         }
 
         @Override
         protected void onPostExecute(Void result) {
+            //hide the download bar since download is finished
+            dl_progress.setVisibility(View.INVISIBLE);
+
+            //update button text to communicate to the user that the audiobook has downloaded
+            dl_btn.setText(R.string.delete_txt);
         }
     }
 }
